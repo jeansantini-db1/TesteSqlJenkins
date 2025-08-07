763acb7a30f12dbc2e3122d35a8d016e3114f11c
jeansantini-db1 <111289185+jeansantini-db1@users.noreply.github.com>
2025-08-07 14:40:34 -0300

    Delete sql/Create endereco.sql

--- sql/Create endereco.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE ENDERECO (
-    id              INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    rua             VARCHAR2(50),
-    numero          NUMBER,
-    ativo           NUMBER(1)
-);
