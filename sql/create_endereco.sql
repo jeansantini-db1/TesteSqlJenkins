4351e9ddb1b7c3845901409196309754272e8ced
Jean Francisco Soares Santini <jean.santini@db1.com.br>
2025-08-07 15:12:10 -0300

    [TEST-001] create tables

---
+++ sql/create_endereco.sql
@@ -0,0 +1,18 @@
+DECLARE
+  v_table_exists NUMBER;
+BEGIN
+
+  SELECT COUNT(*) INTO v_table_exists
+  FROM user_tables
+  WHERE table_name = 'ENDERECO';
+
+  IF v_table_exists = 0 THEN
+    EXECUTE IMMEDIATE 'CREATE TABLE ENDERECO (
+      id              INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+      rua             VARCHAR2(50),
+      numero          NUMBER,
+      ativo           NUMBER(1)
+    );';
+  END IF;
+END;
+/
