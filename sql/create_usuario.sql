9d642a614d2b22124c64c4bb30b9fa1ec890f031
Jean Francisco Soares Santini <jean.santini@db1.com.br>
2025-08-07 14:52:22 -0300

    [TEST-001] CREATE TABLES

---
+++ sql/create_usuario.sql
@@ -0,0 +1,18 @@
+DECLARE
+  v_table_exists NUMBER;
+BEGIN
+  SELECT COUNT(*) INTO v_table_exists
+  FROM user_tables
+  WHERE table_name = 'USUARIO';
+
+  IF v_table_exists = 0 THEN
+    EXECUTE IMMEDIATE 'CREATE TABLE USUARIO (
+      id              INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+      nome            VARCHAR2(50),
+      documento       VARCHAR(20),
+      dataNascimento  DATE,
+      ativo           NUMBER(1)
+    );';
+END IF;
+END;
+/
